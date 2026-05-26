4727df4cb0b05da53537f152abe816d94b6e16fa
Safiatoutelly <safiatoutelly685@gmail.com>
2026-05-26 01:48:46 +0000

    fix: remove app.options line from app.js

--- src/app.js
+++ src/app.js
@@ -43,8 +43,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
 }));
 
-// ✅ Preflight pour toutes les routes
-app.options('*', cors());
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
